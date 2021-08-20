3f52cc20760203df66e80c83153ce6cc86e5d925
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-08-20 13:14:29 +0900

    キューブとスクリプト

---
+++ Assets/Nanika.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nanika : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ぱん作った");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
