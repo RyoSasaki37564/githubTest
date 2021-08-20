ec4d3f83a1785bcc25005d804ce764e7ee765116
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-08-20 13:20:02 +0900

    スクリプトに変更を加えた

--- Assets/Nanika.cs
+++ Assets/Nanika.cs
@@ -7,12 +7,12 @@ public class Nanika : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("ぱん作った");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("ぱん作った");
     }
 }
